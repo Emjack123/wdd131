0e7e556ed0ee05c477b9b98650234b4db505ea90
Jackson Emaeyak <ejackson6@byupathway.edu>
2026-04-11 20:22:54 +0100

    big faith

--- project/scripts/filtered-edifice.js
+++ project/scripts/filtered-edifice.js
@@ -1,4 +1,4 @@
-const mainNav = document.querySelector('.navigation')
+const mainNav = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
 
 hamButton.addEventListener('click', () => {
